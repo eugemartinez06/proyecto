00259463d34f47ee15ff02df19a672c6794fe97b
eugemartinez06 <eugemartinez06@gmail.com>
2024-05-23 12:26:03 -0300

    agregandoparques

---
+++ script.js
@@ -0,0 +1,7 @@
+/*let nombre = prompt("Ingresá tu Nombre");
+function saludo (){
+    if (nombre){
+        alert("Bienvenid@ a Argentina " + nombre);
+         }
+        }
+            saludo();*/
